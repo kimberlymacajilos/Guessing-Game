68a69f3d233fc4b3655d484ba43c9e6d4a6abe78
kimberlymacajilos <202280322@psu.palawan.edu.ph>
2024-04-30 23:10:38 +0800

    Added second feature to server

--- gserver.py
+++ gserver.py
@@ -7,8 +7,13 @@ banner = """
 == Guessing Game v1.0 ==
 Enter your guess:"""
 
-def generate_random_int(low, high):
-    return random.randint(low, high)
+def generate_random_int(difficulty):
+    if difficulty == "easy":
+        return random.randint(1, 50)
+    elif difficulty == "medium":
+        return random.randint(1, 100)
+    elif difficulty == "hard":
+        return random.randint(1, 500)
 
 # initialize the socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,10 +27,12 @@ while True:
     if conn is None:
         print("waiting for connection..")
         conn, addr = s.accept()
-        guessme = generate_random_int(1,100)
         print(f"new client: {addr[0]}")
+        conn.sendall(b"Choose difficulty level: easy (1-50), medium (1-100), hard (1-500): ")
+        difficulty = conn.recv(1024).decode().strip().lower()
         # cheat_str = f"==== number to guess is {guessme} \n" + banner 
         # conn.sendall(cheat_str.encode())
+        guessme = generate_random_int(difficulty)
         conn.sendall(banner.encode())
     else:
         client_input = conn.recv(1024)
