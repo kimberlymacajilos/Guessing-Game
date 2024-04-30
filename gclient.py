532dae663c546f9d25925e2e3b284b18d6d3b733
kimberlymacajilos <202280322@psu.palawan.edu.ph>
2024-04-30 19:33:40 +0800

    Added first feature

--- gclient.py
+++ gclient.py
@@ -1,27 +1,31 @@
 import socket
 
 
-host = "10.125.41.154"
+host = "localhost"
 port = 7777
 
-
-s = socket.socket()
-s.connect((host, port))
-
-# received the banner
-data = s.recv(1024)
-# print banner
-print(data.decode().strip())
-
 while True:
-    #let get our input from the user
-    user_input = input("").strip()
+    s = socket.socket()
+    s.connect((host, port))
 
-    s.sendall(user_input.encode())
-    reply = s.recv(1024).decode().strip()
-    if "Correct" in reply:
+    # received the banner
+    data = s.recv(1024)
+    # print banner
+    print(data.decode().strip())
+
+    while True:
+        #let get our input from the user
+        user_input = input("").strip()
+
+        s.sendall(user_input.encode())
+        reply = s.recv(1024).decode().strip()
+        if "Correct" in reply:
+            print(reply)
+            break
         print(reply)
+        continue
+    s.close()
+
+    again = input("Do you to play again? Enter (y)/(n) ").strip().lower()
+    if again != "y":
         break
-    print(reply)
-    continue
-s.close()
